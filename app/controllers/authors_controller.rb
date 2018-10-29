5c62db5681ea7d8d14d4b3e74211b93b2a6dfbbb
Leon Matthes <leon@matthes.biz>
2018-10-29 17:20:43 +0100

    Added strong parameters to #5

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,9 +3,10 @@ class AuthorsController < ApplicationController
     end
 
     def create
+        params.require(:author).permit(:first_name, :last_name, :homepage)
         @author = Author.new(
-            first_name: params[:first_name], 
-            last_name: params[:last_name], 
+            first_name: params[:first_name],
+            last_name: params[:last_name],
             homepage: params[:homepage])
 
         @author.save
