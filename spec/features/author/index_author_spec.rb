8eb59c5f8570cc7a6025065fa6481193c22a0227
Leon Matthes <leon@matthes.biz>
2018-11-02 15:32:26 +0100

    Working on #

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 describe 'Author index page' do
 
-  it 'should render' do
+  before() do
+    @author = FactoryBot.create(:author)
     visit authors_path
   end
 
-  it 'should contain a table' do
-    author = FactoryBot.create(:author)
-    visit authors_path
-    expect(page).to have_text author.name
-    expect(page).to have_text author.homepage
+  it 'should contain names and homepages of authors' do
+    expect(page).to have_text @author.name
+    expect(page).to have_text @author.homepage
+  end
+
+  it 'should link to detail page' do
+    expect(page).to have_link nil, href: author_path(@author)
+  end
+
+  it 'should link to new page' do
+    expect(page).to have_link nil, href: new_author_path
   end
 end
