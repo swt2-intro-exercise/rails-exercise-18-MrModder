8f0c6ed651334da2dbbd115e8a7019d65415acd0
Leon Matthes <leon@matthes.biz>
2018-11-02 15:48:31 +0100

    Working on #

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 
 describe "Author model", type: :model do
 
-    it "can be created" do
-        author = Author.new(
-            first_name: "Alan", 
-            last_name: "Turing", 
-            homepage: "http://wikipedia.org/Alan_Turing")
-        expect(author.first_name).to eq("Alan")
-        expect(author.last_name).to eq("Turing")
-        expect(author.name).to eq("Alan Turing")
-        expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
-    end
+  it "can be created" do
+    author = Author.new(
+        first_name: "Alan",
+        last_name: "Turing",
+        homepage: "http://wikipedia.org/Alan_Turing")
+    expect(author.first_name).to eq("Alan")
+    expect(author.last_name).to eq("Turing")
+    expect(author.name).to eq("Alan Turing")
+    expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+    expect(author).to be_valid
+  end
+
+  it 'is not valid without last name' do
+    author = Author.new(first_name: 'Alan')
+    expect(author).to_not be_valid
+
+    author2 = Author.new(first_name: 'Alan', last_name: '')
+    expect(author).to_not be_valid
+  end
 end
