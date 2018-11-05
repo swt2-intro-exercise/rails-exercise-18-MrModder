77c262d45960789808595e31f10908a8aa9ed12a
Leon Matthes <leon@matthes.biz>
2018-11-05 11:55:23 +0100

    Working on #15

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,5 @@
 class Paper < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 1}
+  validates :venue, presence: true, length: {minimum: 1}
+  validates :year, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
