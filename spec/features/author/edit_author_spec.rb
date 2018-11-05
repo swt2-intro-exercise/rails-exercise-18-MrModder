30e7eebfb4fe55eb7077c453170d6729ee1c6f71
Leon Matthes <leon@matthes.biz>
2018-11-05 11:26:51 +0100

    Working on #12

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -6,4 +6,19 @@ describe 'Author edit page' do
     author = FactoryBot.create(:author)
     visit edit_author_path(author)
   end
+
+  it 'should change author on submit' do
+    author = FactoryBot.create(:author)
+    visit edit_author_path(author)
+
+    first_name = author.first_name + 'some string'
+    expect(author.first_name).to_not eq first_name
+
+    page.fill_in 'author[first_name]', with: first_name
+    find('input[type = "submit"]').click
+    author.reload
+
+    expect(author.first_name).to eq first_name
+
+  end
 end
